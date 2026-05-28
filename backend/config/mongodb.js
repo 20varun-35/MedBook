73a4280aed3a25f3e22bdadb00449ebc26544dcb
Rishika <ramaswamirishika@gmail.com>
2026-05-28 21:38:08 +0530

    updated mongodb.js

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-
-    mongoose.connection.on('connected', () => console.log("Database Connected"))
-    await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`)
-
-}
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        mongoose.connection.on('connected', () => console.log("Database Connected"));
+        console.log("MongoDB connected successfully");
+    } catch (err) {
+        console.error("MongoDB connection error:", err);
+    }
+};
 
 export default connectDB;
-
